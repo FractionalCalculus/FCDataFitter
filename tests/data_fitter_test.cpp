f20dcbfecb2fe70cbf97c3a1aecc5ec6420a71ec
Abbas Palash <abbas.palash@gmail.com>
2021-05-01 16:42:32 +0430

    googletest added localy, windows platform specific .bazelrc introduced to make it dynamic

--- tests/data_fitter_test.cpp
+++ tests/data_fitter_test.cpp
@@ -1,9 +1,5 @@
-#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
-// Demonstrate some basic assertions.
 TEST(DataFitter, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+
 }
